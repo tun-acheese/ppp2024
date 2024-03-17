19ca33c4767f788dbc23c9356fffce4287c76053
tun-acheese <153492430+tun-acheese@users.noreply.github.com>
2024-03-17 11:37:08 +0900

    Update hw05.py

--- hw05.py
+++ hw05.py
@@ -1,1 +1,10 @@
+fruit =input("과일 종류를 입력하시오.")
+if fruit=='한라봉':
+   print('50kcal /100g')
+elif fruit=='딸기(설향)':
+   print('34kcal /100g')
+elif fruit=='바나나':
+   print('77kcal /100g')
+else:
+    print('잘 모르겠습니다')   
 
