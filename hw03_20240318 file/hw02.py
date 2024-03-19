1bd22a587c20bba94136ad7e4a56c20c996ffb4a
tun-acheese <153492430+tun-acheese@users.noreply.github.com>
2024-03-19 11:48:26 +0900

    Update hw02.py
    수정

--- hw03_20240318 file/hw02.py
+++ hw03_20240318 file/hw02.py
@@ -1,10 +1,11 @@
-def solution (dot):
-    if dot [0] > 0 and dot [1]>0:
-       answer = 1
-    elif dot [0] < 0 and dot [1]>0:
-       answer = 2
-    elif dot [0] < 0 and dot [1]<0:
-       answer = 3
-    else:
-       answer = 4
-    return answer
+x=int(input("x좌표를 입력하시오."))
+y=int(input("y좌표를 입력하시오."))
+
+if x>0 and y>0:
+    print("1")
+elif x>0 and y<0:
+    print("4")
+elif x<0 and y>0:
+    print("2")
+else:
+    print("3")
