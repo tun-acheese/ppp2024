de6016162f69cdef1d613e5b0f2a30b5bf3194c3
tun-acheese <153492430+tun-acheese@users.noreply.github.com>
2024-03-20 06:43:31 +0000

    수정

---
+++ lec04_20240320/lec01.py
@@ -0,0 +1,56 @@
+#1-10까지 합
+
+
+
+total = 0
+
+total = 1+2+3+4+5+6+7+8+9+10
+
+total = total + 1
+total = total + 2
+total = total + 3
+total = total + 4
+total = total + 5
+total = total + 6
+total = total + 7
+total = total + 8
+total = total + 9
+total = total + 10
+
+print(f"1부터 10까지 합은 {total}입니다.")
+
+total=0
+for i in range(10):
+    total = totla +(i+1)
+
+print(f"1부터 10까지 합은 {total1}입니다.")
+print(f"1부터 10까지 합은 {total2}입니다.")
+print(f"1부터 10까지 합은 {total3}입니다.")
+
+
+# 1-10까지의 합
+total = 0
+
+totla 1 = 1+2+3+4+5+6+7+8+9+10
++11+12+13+14+15+16
+
+total = 0
+for i in range(100):
+    total - totla + (i+1)
+
+# 1-1000까지의 합
+n = 1000
+total = 0
+for i in range(n):
+    total = total + (i+1)
+
+print(f"1부터 {n}까지 합은 {total:,}입니다.")
+
+#1-n까지 짝수합
+n = 250
+total = 0
+for i in range(n):
+    if (i+1)% 2== 0:
+        total = total + (i+1)
+
+print(f"1부터 {n}까지 합은 {total:,}입니다.")
