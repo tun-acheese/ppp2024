5f7fd519c44b78eca26a13f107ae19315b46c117
tun-acheese <153492430+tun-acheese@users.noreply.github.com>
2024-03-25 06:42:30 +0000

    ㅇ

--- hw06color_text.py
+++ hw06color_text.py
@@ -1,8 +1,12 @@
 def colored(r,g,b, text):
     return "\033[38;2;{};{};{}m{}\033[38;2;255;255;255m".format(r,g,b,text)]]"
 
-def hello(name):
+def colored_name(name, text):
+    if name == "red":
+        return "\033[38;2;{};{};{}m{}\033[38;2;255;255;255m".format(255,)]]"
+]"
     print(f"{colored(0,0m,255,name}님 안녕하세요.")
+
 
 print(colored(0,255,0,'Hello, World'))
 hello("홍길동")
---
+++ hw07_f3c.py
@@ -0,0 +1,6 @@
+#4주차 코드
+# \def f2c(temp_f):
+    return (temp_f - 32)* 5 / 9
+
+temp_f = 100
+print("{}f => {}c".format(temp_f,f2c(temp_f)))
