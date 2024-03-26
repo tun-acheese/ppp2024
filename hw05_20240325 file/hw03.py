1e725aa8d8c4ce5b237b84fdabe48d699e65bd9b
tun-acheese <153492430+tun-acheese@users.noreply.github.com>
2024-03-26 20:48:15 +0000

    l

--- hw05_20240325 file/hw03.py
+++ hw05_20240325 file/hw03.py
@@ -1,7 +1,12 @@
 def main():
-temp_c=int(input('섭씨 온도를 입력하세요:'))
-    temp_f=(temp_c*1.8)+32
-    print(f'섭씨온도 {temp_c}는 화씨 온도로 {temp_f}입니다')
+def c2f(t_c):
+    f = t_c * (9/5) +32
+    return f
+
+print(c2f(10))
+print(c2f(20))
+print(c2f(30))
+
 
 if_name_=="_main_":
     main()
