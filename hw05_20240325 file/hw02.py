2420ec2b3e79a79260524fa8d33c9ebf5de00616
tun-acheese <153492430+tun-acheese@users.noreply.github.com>
2024-03-26 19:45:45 +0000

    k

--- hw05_20240325 file/hw02.py
+++ hw05_20240325 file/hw02.py
@@ -1,8 +1,12 @@
-num_string = input("구구단 몇 단을 출력할까요?")
-number = int(num_string)
-gugudan_dan = str.format("구구단 {}단을 출력합니다.",number)
-print(gugudan_dan)
-for i in range(1,10):
-    result = number * 1
-    result_string = str. format("{} * {} = {}", number, i , result)
-    print(result_string)
+def main():
+    num_string = input("구구단 몇 단을 출력할까요?")
+    number = int(num_string)
+    gugudan_dan = str.format("구구단 {}단을 출력합니다.",number)
+    print(gugudan_dan)
+    for i in range(1,10):
+        result = number * 1
+        result_string = str. format("{} * {} = {}", number, i , result)
+        print(result_string)
+
+if_name_=="_main_":
+    main()
