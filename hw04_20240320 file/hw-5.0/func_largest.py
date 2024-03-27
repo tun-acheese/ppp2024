d6f3d4e11f9f10267fa5ea8641293c72b11ad870
tun-acheese <153492430+tun-acheese@users.noreply.github.com>
2024-03-27 05:48:25 +0000

    h

---
+++ hw04_20240320 file/hw-5.0/func_largest.py
@@ -0,0 +1,14 @@
+def largest (a,b,c,):
+    pass
+
+def main():
+    x1 = 5
+    X2 = 7
+    X3 = 3
+
+    largest_num = largest(x1, X2, X3)
+
+    print(f"가장 큰 수는{largest_num}입니다.
+          
+if_name_=="_main_":
+    main()
