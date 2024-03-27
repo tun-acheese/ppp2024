6ad4dc03ff9538669f698add65617708b44b2ec5
tun-acheese <153492430+tun-acheese@users.noreply.github.com>
2024-03-27 06:09:42 +0000

    ㅇ

--- hw06_20240327 file/func_largest_list.py
+++ hw06_20240327 file/func_largest_list.py
@@ -1,7 +1,11 @@
-def main():
-    x = [3,7,5]
 
-    print(f"최대값은 {largest(x)}입니다.")
+def largest(nums):
+    pass
+
+def main():
+    x = [3,7,5,6,3]
+    print(f"최대값은 {largest(X)입니다.}")
+
 
 if __name__ == "__main__":
     main()
