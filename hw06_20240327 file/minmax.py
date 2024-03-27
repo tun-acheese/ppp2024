cefdb2b7d78292015bdb8a2745d1927d5f1fab0f
tun-acheese <153492430+tun-acheese@users.noreply.github.com>
2024-03-27 06:25:08 +0000

    d

---
+++ hw06_20240327 file/minmax.py
@@ -0,0 +1,11 @@
+def minmax(nums):
+    pass
+
+def main():
+    nums = [3, 5, 7, 4, 10, 2]
+    mn, mx = minmax(nums)
+
+    print(f"최솟값은 {mn}, 최댓값은 {mx}")
+
+if __name__ == "__main__ ":
+    main()
