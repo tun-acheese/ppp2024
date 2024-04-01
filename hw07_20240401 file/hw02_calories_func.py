6c5539f9a08023952d4f6e76303e52c95ee9f2d3
tun-acheese <153492430+tun-acheese@users.noreply.github.com>
2024-04-01 06:14:45 +0000

    d

--- hw07_20240401 file/hw02_calories_func.py
+++ hw07_20240401 file/hw02_calories_func.py
@@ -2,9 +2,9 @@ def total_calorie(fruits_wed, fruits):
     return 0.0
 
 def main():
-    fruit_caloris_dic=("한라봉": 50, "딸기": 34, "바나나":77)
+    fruit_calorie_dic=("한라봉": 50, "딸기": 34, "바나나":77)
     fruits_mon={"딸기": 300, "한라봉": 150}
-    print(total_calorie(fruits_mon, fruits_calories_dic))
+    print(total_calorie(fruits_mon, fruits_calorie_dic))
 
     fruits_wed={"딸기": 200, "바나나": 300}
     print(total_calorie(fruits_wed, fruits_calorie_dic))
