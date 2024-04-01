3ae9a3a725c3029fd02b3ce4482addc09e4b4f01
tun-acheese <153492430+tun-acheese@users.noreply.github.com>
2024-04-01 05:35:23 +0000

    d

---
+++ hw07_20240401 file/hw01_averager.py
@@ -0,0 +1,13 @@
+def average(nums):
+    # return sum(nums)/len(nums)
+    total = 0
+    count = 0
+    for num in nums:
+        total += num
+        count +=1
+
+        return total / count
+    
+def main():
+    numbers=[3, 5, 10, 15, 7]
+    print(f"주어진 리스트의 평균은 {average(nums):.1f}입니다.")
