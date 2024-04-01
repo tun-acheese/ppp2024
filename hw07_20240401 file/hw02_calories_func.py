4b20b9f88acbdf593d7a875ec81a82855962f82d
tun-acheese <153492430+tun-acheese@users.noreply.github.com>
2024-04-01 06:25:35 +0000

    d

--- hw07_20240401 file/hw02_calories_func.py
+++ hw07_20240401 file/hw02_calories_func.py
@@ -5,10 +5,14 @@ def total_calorie(fruits_eat, fruits_cal_dic):
 
     #방법2
     total=0
-    for fruit_name, fruit_gram in fruits_eat:
+    for fruit_name, fruit_gram in fruits_eat.item():
         print(fruit_name)
-        fofla += fruits_eat[fruit_name] * fruits_cal_dic[fruits_name] / 100
+        total += fruits_eat[fruit_name] * fruits_cal_dic[fruits_name] / 100
     return total 
+
+    #방법2
+    return sum[gram*fruits_cal_dic[name]/100
+                for name, gram in fruits_eat.items()])
 
 def main():
     fruit_calorie_dic=("한라봉": 50, "딸기": 34, "바나나":77)
