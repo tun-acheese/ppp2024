9a06b0a87a4abfc6626b8daf02c4756db8cb58ee
tun-acheese <153492430+tun-acheese@users.noreply.github.com>
2024-04-01 06:40:41 +0000

    d

--- hw07_20240401 file/hw03_test_spirt.py
+++ hw07_20240401 file/hw03_test_spirt.py
@@ -1,10 +1,11 @@
 def main():
     inpu_text = "5, 10 ,3 ,4 ,7"
     tokens= = input_text.split(",")
+    # print(sum(tokens))
     results = []
     for token in tokens:
         results.append(int(token))
-    print(results)
+    print(sum(results))
  \
 if __name__ == "__main__":
     main():
