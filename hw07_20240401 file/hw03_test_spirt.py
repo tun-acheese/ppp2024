8b8ce676d6397ba76e1c12de379237212e1b6f08
tun-acheese <153492430+tun-acheese@users.noreply.github.com>
2024-04-01 06:39:42 +0000

    d

---
+++ hw07_20240401 file/hw03_test_spirt.py
@@ -0,0 +1,10 @@
+def main():
+    inpu_text = "5, 10 ,3 ,4 ,7"
+    tokens= = input_text.split(",")
+    results = []
+    for token in tokens:
+        results.append(int(token))
+    print(results)
+ \
+if __name__ == "__main__":
+    main():
