75ebe521b7e455d7a496c9a80a3a8951a8222d64
tun-acheese <153492430+tun-acheese@users.noreply.github.com>
2024-04-02 21:10:17 +0000

    ㅣk

--- hw07_20240401 file/hw01_text2list.py
+++ hw07_20240401 file/hw01_text2list.py
@@ -5,7 +5,13 @@ def main():
     print("평균값은{:.1}".format(average(nums))
     print("중앙값은 {}".format(median(nums))
     #단 갯수가 짝수일때는 중앙에 위치한 두 값 중 큰 값을 채택한다
-    print(f"최솟값은{}")
-    print(f"최댓값은{}")
+    if len(nums) % 2 ==0:
+    print(nums[len(nums)//2)])
+    else len(nums) % 2 !=0:
+    print(nums[len(nums)//2+1)])
+    print(f"최솟값은max{nums}")
+    #최댓값
+    print(f"최댓값은max{nums}")
+
 if__name__=="__main__": 
     main()
