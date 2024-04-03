ad83f2f35ab28386d059e83dce769a5ae3bd797c
tun-acheese <153492430+tun-acheese@users.noreply.github.com>
2024-04-03 05:33:27 +0000

    jk

--- lec08_20240403/ex_flie_input.py
+++ lec08_20240403/ex_flie_input.py
@@ -6,5 +6,21 @@ def average(nums):
 
 def median(nums):
     return nums[len(nums)//2]
+
+def read_fiel(filename):
+    return" "
+
 def main(): 
     input_text = "5 10 3 4 7"
+    input_text = read_file("numver1.txt")
+
+
+    nums = text2list(input_text)
+
+    print(f"학생 {i} 결과:")
+    print("주어진 리스트는", nums)
+    print(f"평균값은 {average(nums)}:.1f}")
+    print("중앙값은 {}".format(median(nums)))
+    print("최솟값은 {}".format(min(nums)))
+    print("최댓값은 {}".format(max(nums)))
+    print(f"리스트의 마지막 값은 {nums[-1]}")
