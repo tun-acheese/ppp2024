000ca83964529a152396ce8685405459331b3fdf
tun-acheese <153492430+tun-acheese@users.noreply.github.com>
2024-04-14 02:58:30 +0000

    d

--- hw10_20240408 file/hw02 rainfall.py
+++ hw10_20240408 file/hw02 rainfall.py
@@ -1,21 +1,16 @@
-import csv
-f = open('rainfall.csv'.'r'.encoding='cp949')
-data=csv.reader(f.delimiter-',')
-print(data)
+# 평균 기온 (일평균 기온의 연평균)
+# 5mm이상 강우 일수
+# 총 강우량
+_
 
-data = csv.reader(f)
-next(data)
-low = []
+def read_average_temperature(filename):
+    result=[]
+    with open(filename) as f:
+        lines=f.readlines()
+        for line in lines[1:]:
+            tokens=line.split(",")
+            tavg=float(tokens[4])
+            results.append(tavg)
+    return results
 
-for row in data :
-    if row[-2] !=
-
-
-
-
-평균 기온 (일평균 기온의 연평균)
-5mm이상 강우 일수
-총 강우량
-
-if__name__=="__main__": 
-main()
+    
