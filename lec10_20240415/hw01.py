5004373730b8ed6830f6e173c5100efa4b5d3064
tun-acheese <153492430+tun-acheese@users.noreply.github.com>
2024-04-15 06:17:14 +0000

    ;

--- lec10_20240415/hw01.py
+++ lec10_20240415/hw01.py
@@ -19,8 +19,6 @@ def count_rainday(rainfall)
     return sum([1 if x >= 5 else 0 for x in rainfall])
 
                 
-
-
 def count_rainday(rainfall):
     #rain_day = 0
     #for rain in rainfall:
@@ -28,9 +26,28 @@ def count_rainday(rainfall):
     #       rain_day += 1
      
     #return rain_day
-    return 
+    return  sum([1 if x >= 5 else 0 for x in rainfall])
 
-    def longest_rainday(rainfall):
+def longest_rainday(rainfall):
+    rain_event = []
+
+    prev_rain = 0
+    prev_rain_count = 0
+    for rain in rainfal:
+        if rain == 0
+            if prev_rain_count > 0 :
+                rain_event.append(prev_rain_count)
+            prev_rain_count = 0
+        else:
+            prev_rain_count += 1
+    return max(rain_event)
+
+def top_rank(values, limit):
+    # return sorted(values)[-limit:][::-1]
+    return sorted (values, reverse=True)[:limit]
+
+
+
 
 def main():
     wether_filename = "lec10_20240415/weather(146)_2022-2022.csv"
@@ -47,5 +64,8 @@ def main():
 
     #4번 최장연속 강우일수
     print(f"최장 강우일수는 {longest_rainday(rainfall)}일입니다")
-
+    #5번 강우이벤트 중 최대 강수량
+    print(f"최대 강우일수는 {max_rainfall_event(rainfall):.1f}mm입니다.")
+    #6번 가장 더운날 top3
+    print(f"가장 더운날 top3는 {top_rank(tmax, 3)}mm입니다.")
 if __name__ == "__main__":
