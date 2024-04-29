cfaa1a7a7d8d646edcea8c84a12c662ce2aa5b4d
tun-acheese <153492430+tun-acheese@users.noreply.github.com>
2024-04-29 06:33:07 +0000

    fd

---
+++ lec13_20240429/hw01.py
@@ -0,0 +1,20 @@
+import sys
+if "./" not in sys.path:
+    sys.path.append("./")
+
+from lec09 import hw_submission
+
+def download(filename, url)
+    pass
+
+def main():
+    URL = "http://data.kma.go.kr/stcs/grnd/downloadGrndTalist.do?fileType=csv$"
+    filename 
+    download(filename, URL)
+
+\
+
+tmax,tma_data = 38, "2021-08-12"
+tdiff_max, tdiff_max_date = 28.5, "1989-01-09"
+
+hw_submission.submit("홍길동", tmax_date, tdiff)
