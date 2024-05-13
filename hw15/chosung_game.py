090416d13b43a1fe0f504b3618a3741141ef84d0
tun-acheese <153492430+tun-acheese@users.noreply.github.com>
2024-05-13 05:59:27 +0000

    dㅇ

---
+++ hw15/chosung_game.py
@@ -0,0 +1,27 @@
+
+def get_chosung(text):
+    CHOSUNG_LIST = ['ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅃ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ']
+
+    for gulja in text:
+        print(gulja,ord(gulja) - ord('가'))//588, (ord(*gulja) - ord('가'))%588)
+
+
+    return "ㅍㅇㅅ"
+
+def main:
+    hidden_answer = "프원실"
+    problem = get_chosung(hidden_answer)
+
+
+    hidden_answer = "프원실"
+    problem = get_chosung(hidden_answer)
+    print(f"문제입니다. 주어진 초성은 '{problem}'입니다.")
+
+    answer = input("답은=>?")
+    if answer == hidden_answer:
+        print("정답입니다.")
+    else:
+        print("틀렸습니다.")
+
+    if __name__=="_main_":
+        main()
