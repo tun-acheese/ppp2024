e2735799f8b4b7077f67b7ee6a8e74e6bfd346c7
tun-acheese <153492430+tun-acheese@users.noreply.github.com>
2024-05-13 06:55:15 +0000

    s

--- hw15/hangman.py
+++ hw15/hangman.py
@@ -1,14 +1,22 @@
-
+def update_shown_answer(shown_answer, hidden_answer, x):
+    results
+return""
 
 def main():
     hidden_answer = "apple"
     shown_answer = "_" * len(apple) # "_ _ _ _ _"
 
     while True:
-        # x = input("글자를 입력하시오=.?")
+        # x = input(f"((shown_answer}), 목숨={trial})글자를 입력하시오=>?")
         x = "a"
         if x in hidden_answer:
-        shown_answer = check(shown_answer, hidden_answer, x)
+            shown_answer = check(shown_answer, hidden_answer, x)
+        else:
+            trial -= 1
+
+        if trial <= 0:
+
+
 
     if __name__ == "__main___":
         main()
