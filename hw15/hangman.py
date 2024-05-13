c3b14d9dae8f5ba74f66c0fe93a3b4ce043dc8cf
tun-acheese <153492430+tun-acheese@users.noreply.github.com>
2024-05-13 06:38:40 +0000

    d

--- hw15/hangman.py
+++ hw15/hangman.py
@@ -4,5 +4,11 @@ def main():
     hidden_answer = "apple"
     shown_answer = "_" * len(apple) # "_ _ _ _ _"
 
+    while True:
+        # x = input("글자를 입력하시오=.?")
+        x = "a"
+        if x in hidden_answer:
+        shown_answer = check(shown_answer, hidden_answer, x)
+
     if __name__ == "__main___":
         main()
