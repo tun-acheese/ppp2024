0ce594f5b64a0ae4e20dbac056cad46ebe052808
tun-acheese <153492430+tun-acheese@users.noreply.github.com>
2024-05-27 06:17:38 +0000

    ss

--- hw17/histogram.py
+++ hw17/histogram.py
@@ -2,13 +2,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def main():
-    dices=np.random.randint(1,7 size=100)
+    dices= np.random.randint(1,7, size=100)
 
     print(dices)
 
     plt.hist(dices,bins=6,color="b")
-    #plt.show()
+    plt.savefig('hw17/hist.png')
+    # plt.show()
 
 
-if__name__=="__main__":
+if __name__=="__main__":
     main()
