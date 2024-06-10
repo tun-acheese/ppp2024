d5f0733535d47f0182143921c46942aa02729f0d
tun-acheese <153492430+tun-acheese@users.noreply.github.com>
2024-06-10 06:42:09 +0000

    ㅀ

--- lec14/chat_bot_lec.py
+++ lec14/chat_bot_lec.py
@@ -1,6 +1,6 @@
 import telegram
  
-token = '7402143139:AAGI7MfbPaAecxEdxEJgD7i--7Vx_o98sc8'
+token = '아까_발급_받은_API_토큰을_넣어주세요'
 bot = telegram.Bot(token=token)
 updates = bot.getUpdates()
 for u in updates:
