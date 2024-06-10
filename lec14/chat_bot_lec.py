317a2b97f57762f56e9521bc46aa8f30364889ae
tun-acheese <153492430+tun-acheese@users.noreply.github.com>
2024-06-10 10:38:46 +0000

    텔레그램 버전 변경

--- lec14/chat_bot_lec.py
+++ lec14/chat_bot_lec.py
@@ -1,9 +1,7 @@
 import telegram
  
-token = '아까_발급_받은_API_토큰을_넣어주세요'
+token = '7402143139:AAGI7MfbPaAecxEdxEJgD7i--7Vx_o98sc8'
 bot = telegram.Bot(token=token)
 updates = bot.getUpdates()
 for u in updates:
     print(u.message)
-
-    #7402143139:AAGI7MfbPaAecxEdxEJgD7i--7Vx_o98sc8de
