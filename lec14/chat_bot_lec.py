fa6db1646f44dfc56641b510b8e186c39213cf37
tun-acheese <153492430+tun-acheese@users.noreply.github.com>
2024-06-10 05:54:21 +0000

    r

--- lec14/chat_bot_lec.py
+++ lec14/chat_bot_lec.py
@@ -1,7 +1,7 @@
 import telegram
  
 token = '7402143139:AAGI7MfbPaAecxEdxEJgD7i--7Vx_o98sc8'
-id = ""
- 
-bot = telegram.Bot(token)
-bot.sendMessage(chat_id=id, text="테스트 중입니다.")
+bot = telegram.Bot(token=token)
+updates = bot.getUpdates()
+for u in updates:
+    print(u.message)
