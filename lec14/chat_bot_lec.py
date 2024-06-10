ec3c027efc5f7d541d9c4e8eafe0419d447677d1
tun-acheese <153492430+tun-acheese@users.noreply.github.com>
2024-06-10 05:56:09 +0000

    rtc

--- lec14/chat_bot_lec.py
+++ lec14/chat_bot_lec.py
@@ -1,7 +1,9 @@
 import telegram
  
-token = '7402143139:AAGI7MfbPaAecxEdxEJgD7i--7Vx_o98sc8'
+token = '  '
 bot = telegram.Bot(token=token)
 updates = bot.getUpdates()
 for u in updates:
     print(u.message)
+
+    #7402143139:AAGI7MfbPaAecxEdxEJgD7i--7Vx_o98sc8
