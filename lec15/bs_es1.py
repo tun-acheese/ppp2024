71ef0da4e349eef9fddc7d0a9971c76e24b88fc2
tun-acheese <153492430+tun-acheese@users.noreply.github.com>
2024-06-12 05:32:50 +0000

    s

--- lec15/bs_es1.py
+++ lec15/bs_es1.py
@@ -1,7 +1,7 @@
+from bs4 import BeautifulSoup
+
 def main():
    
-<p class="title"><b>The Dormouse's story</b></p>
-
 <p class="story">Once upon a time there were three little sisters; and their names were
 <a href="http://example.com/elsie" class="sister" id="link1">Elsie</a>,
 <a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
