83c348885d40625894057bcb84f625fadc2ad800
tun-acheese <153492430+tun-acheese@users.noreply.github.com>
2024-06-20 15:41:22 +0000

    xxs

--- hw21_/Chinese_zodiac_sign_bot.py
+++ hw21_/Chinese_zodiac_sign_bot.py
@@ -1,11 +1,8 @@
 import telegram
  
 token = "7402143139:AAGI7MfbPaAecxEdxEJgD7i--7Vx_o98sc8"
-id = "1718895002"
+id = "7028566921"
  
 bot = telegram.Bot(token)
 bot.sendMessage(chat_id=id, text="테스트 중입니다.")
-
-# updates = bot.getUpdates()
-# for u in updates:
-#     print(u.message)
+    
