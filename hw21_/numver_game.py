454855e84400fcd4293a0f1e5938ce228de186aa
tun-acheese <153492430+tun-acheese@users.noreply.github.com>
2024-06-20 19:48:56 +0000

    df

---
+++ hw21_/numver_game.py
@@ -0,0 +1,71 @@
+import random
+num_randeom = random.randint(1,99)
+coin = 10
+# 반복문 만들기(10번 동안 문제를 냄)
+while coin:
+    coin-=1
+    print("남은 코인 개수:{}".format(coin))
+coin = 10
+num = 0
+#조건문 만들기
+while coin:
+    coin -= 1
+    num = int(input("1~99 사이의 숫자를 맞춰보세요"))
+    if num == num_random:
+        print("축하합니다. {}이 정답입니다".format(num))
+        break
+    elif num > num_random:
+        print("{}보다 작습니다. 남은 코인" {}개".format(num,coin))
+    elif num < num_random:
+        print("{}보다 큽니다. 남은 코인: {}개".format(num,coin))
+
+if num != num_random and coin==0:
+    print ("코인이 다 떨어졌습다. 숫자 맞추기 실패입니다.")
+
+#예외
+def input_check(casting = int) -> int:
+    while Tru:
+        try:
+            var = input ("정담은?")
+            var = casting(var)
+            reuturn var
+            break
+        except:
+            print("잘못된 입력입니다. 정수를 입력해주세요.")
+            continue
+#
+def input_check(casting = int) -> int:
+    while 1:
+        try:
+            var = input("정답은?")
+            if var == "exit":
+                return "exit"
+                break
+            else:
+                var = casting(var)
+                return var
+                break
+        except:
+            print("잘못된 입력입니다. 정수를 입력해주세요.")
+            continue
+#
+while coin:
+    num = input_check()
+    coin -= 1
+    if num == "exit":
+        print("게임을 포기하시다니 아쉽습니다.")
+        break
+    elif num == num_random:
+        print("축하합니다. {}이 정답입니다".format(num))
+        break
+    elif num > num_random:
+        print("{}보다 작습니다. 남은 코인: {}개".format(num,coin))
+    elif num < num_random:
+        print("{}보다 큽니다. 남은 코인: {}개".format(num,coin))
+
+    if num != num_random and coin==0:
+        print("코인이 다 떨어졌습니다. 숫자 맞추기 실패입니다.")import random
+
+
+             
+                  
